d34e91b49d7ee08780e4ba36412eb07c9bc074bf
huyfitech <huy_duong@theinfitech.com>
2025-10-29 17:32:33 +0700

    29_10

--- src/Components/Header/Navigation/index.js
+++ src/Components/Header/Navigation/index.js
@@ -44,7 +44,7 @@ const Navigation = () => {
                                   </div>
                               
                               
-                              
+                               
                               
                               
                               </li>
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -36,7 +36,75 @@ const Home = () => {
                 </Button>
               </div>
 
-              <div className="product_rom w-100 mt-3">
+              <div className="product_rom w-100 mt-6">
+                <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      slidesPerView={4}
+      spaceBetween={10}
+      navigation
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 2500, disableOnInteraction: false }}
+    >
+      <SwiperSlide >
+
+
+        <ProductItem/>
+      </SwiperSlide>
+
+            <SwiperSlide >
+
+
+        <ProductItem/>
+      </SwiperSlide>
+
+            <SwiperSlide >
+
+
+        <ProductItem/>
+      </SwiperSlide>
+
+            <SwiperSlide >
+
+
+        <ProductItem/>
+      </SwiperSlide>
+
+            <SwiperSlide >
+
+
+        <ProductItem/>
+      </SwiperSlide>
+
+            <SwiperSlide >
+
+
+        <ProductItem/>
+      </SwiperSlide>
+
+
+
+
+
+
+     
+
+
+    </Swiper>
+              </div>
+
+                            <div className="d-flex align-items-center justify-content-between">
+                <div className="info w-75">
+                  <h3 className="mb-0 hd">Best sellers</h3>
+                  <p className="text-light text-sml mb-0">
+                    Do not miss the current offers until the end of March
+                  </p>
+                </div>
+                <Button className="viewAllBtn">
+                  View All <IoMdArrowForward className="ml-2" />
+                </Button>
+              </div>
+
+              <div className="product_rom w-100 mt-6">
                 <Swiper
       modules={[Navigation, Pagination, Autoplay]}
       slidesPerView={4}
